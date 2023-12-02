176eeff4270238ce54acd740508d69007b5742a3
Thomas Niu <thomasniu@gmail.com>
2023-12-02 21:55:44 +0800

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/niutingyuan/puppy
 
 go 1.21.4
+
+require github.com/niutingyuan/cat v0.0.0-20231202132931-886bbc4b8c36
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
+import "github.com/niutingyuan/cat"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return cat.WhenGrownUp(Bark())
+}
+func BigBarks() string {
+	return cat.WhenGrownUp(Barks())
 }
